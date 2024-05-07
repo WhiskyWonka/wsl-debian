878e590d3b58d9abe04f121504e0cea47cbf35dc
Richard Grinberg <rggrinberg@gmail.com>
2024-05-07 10:12:28 -0300

    nuevo config

--- ohmyz/aliases.sh
+++ ohmyz/aliases.sh
@@ -7,6 +7,7 @@ tailf(){ tail -f $1 | bat --paging=never -l log; }
 # flush all keys in redis-test container
 alias flushall='docker exec redis-test redis-cli FLUSHALL; docker exec php-test php artisan config:clear; docker exec php-test php artisan route:clear; docker exec php-test php artisan cache:clear; docker exec php-test php artisan view:clear'
 # get into dev AWS server
-alias sshdev='ssh -i /mnt/rgg-lenovo/SERVIDORES/DEV\ AWS/keys/aws_boldt_bplay_bplaybet_development ubuntu@44.215.207.90'
-alias sshuat='ssh -i ssh -i /mnt/rgg-lenovo/SERVIDORES/UAT\ AWS/keys/aws_boldt-bplay-bplaybet-qa_web_key.pem ubuntu@34.203.37.180'
+alias sshdev='ssh -i ~/ssh_keys/dev_aws/aws_boldt_bplay_bplaybet_development ubuntu@44.215.207.90'
+alias sshdev='ssh -i ~/ssh_keys/dev_aws/aws_boldt_bplay_bplaybet_development ubuntu@44.215.207.90'
+alias sshuat='ssh -i ~/ssh_keys/uat/aws_boldt-bplay-bplaybet-qa_web_key.pem ubuntu@34.203.37.180'
 alias tailc='~/.config/tailc.sh'
